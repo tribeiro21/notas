8c11cea4724454b0fb3a5f79ccc5c35b74b2fb59
tribeiro21 <tribeiro2109@gmail.com>
2023-10-26 13:12:03 +0200

    Herramienta input

--- notas.py
+++ notas.py
@@ -2,3 +2,7 @@ print("Soy Tony Ribeiro")
 print("Estoy programando con Python")
 
 nota = "Este es mi primer programa"
+
+print("Evalúa la calidad de mi primer programa del 1 al 10")
+nota = input()
+print("La puntuación que me has dado es: "+nota)
